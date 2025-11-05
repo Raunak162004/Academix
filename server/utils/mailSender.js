e4c4f2afa57a72011cd0e2308e55c9a044757b4e
Raunak Malhotra <rm1273503@gmail.com>
2025-11-05 20:47:13 +0530

    done with the mail issue

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -4,7 +4,7 @@ dotenv.config();
 const mailSender = async(email, title, body)=>{
     try{
         let transporter = nodemailer.createTransport({
-            host: process.env.MAIL_HOST,
+            service: "gmail",
             auth:{
                 user: process.env.MAIL_USER,
                 pass: process.env.MAIL_PASS,
